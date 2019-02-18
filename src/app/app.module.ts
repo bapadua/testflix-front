24fa3cdd27a52593d7a9426c3315f6bedc92d2fb
Bruno A. Padua <solucaoelogica@gmail.com>
2019-02-18 06:30:41 -0300

    projeto criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieRowComponent } from './movie-row/movie-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieRowComponent
   ],
   imports: [
     BrowserModule,
